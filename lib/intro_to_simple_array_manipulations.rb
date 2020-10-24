95b010cdce4a98d91b3791f6a096c8e8211585eb
Sin Wildy <sw.wildy@gmail.com>
2020-10-24 02:52:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,6 @@ end
 def pop_with_args(array)
   remove_last = array.pop
   remove_2nd_last = array.pop
-  return remove_last && 
-   remove_2nd_last
+  #return remove_last && 
+  # remove_2nd_last
 end
