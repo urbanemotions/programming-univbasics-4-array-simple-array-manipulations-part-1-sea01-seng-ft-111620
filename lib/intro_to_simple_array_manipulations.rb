16a8f77c42a235054968098e6ff58a5e29c5fdea
Sin Wildy <sw.wildy@gmail.com>
2020-10-24 02:34:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,15 +1,12 @@
 def using_push(array, string)
   array.push(string)
-  #p array
 end
 
 def using_unshift(array, string)
   array.unshift(string)
-  #p array
-  #p array.length
 end
 
 def using_pop(array, string)
   array.pop(string)
-  p array
+  p array[-1]
 end
